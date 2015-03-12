feeab4cd3014db8c55e86fee5006953fb89039c9
Aleksandar Nikolix <aleksandarnikolix@mbpro.lan>
2015-03-12 19:01:26 +0100

    enable traps

--- Dyninst-8.2.1/cmake/cap_arch_def.cmake
+++ Dyninst-8.2.1/cmake/cap_arch_def.cmake
@@ -100,6 +100,8 @@ elseif (PLATFORM STREQUAL i386-unknown-nt4.0)
 set (OS_DEFINES -Dos_windows)
 set (CAP_DEFINES ${CAP_DEFINES} 
              -Dcap_mem_emulation
+              -Dcap_mutatee_traps
+
     )
 endif (PLATFORM MATCHES linux)
 
